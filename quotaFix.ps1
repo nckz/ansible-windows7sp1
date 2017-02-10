9a566352fb703ef8ce60d767219f250ab72995d9
nckz <nick.zwart@gmail.com>
2017-02-10 16:36:21 -0700

    some info on increasing the shell quota

--- quotaFix.ps1
+++ quotaFix.ps1
@@ -1,4 +1,6 @@
 #Requires -Version 3.0
 
+# Increase the number of allowed processes per remote session to 10,000.
+# Increase the allowed memory usage to 8GB.
 winrm set winrm/config/winrs '@{MaxProcessesPerShell="10000"}'
 winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="8000"}'
