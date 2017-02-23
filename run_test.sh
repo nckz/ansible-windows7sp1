d03e93289c62a81fcb9c685f0a0c8992e2ad0b08
nckz <nick.zwart@gmail.com>
2017-02-23 11:14:23 -0700

    some info for older versions of Ubuntu

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# Remember to install python python-pip pywinrm
+#
+# If you are running Ubuntu 12 and have an older version of python pip then 
+# uninstall it:
+#   $ sudo apt-get remove python-pip
+#   $ sudo apt-get autoremove
+#   $ wget https://bootstrap.pypa.io/get-pip.py
+#   $ sudo python ./get-pip.py
+
+
 ansible-playbook test.yml -i ./HOSTS
